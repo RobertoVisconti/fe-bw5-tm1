1fff1d9656873a0036a045fe59c23336be004b39
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-07-23 13:30:56 +0200

    create cartelle per i singoli reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
-import { clientiSlice } from "./clientiSlice"
+import { clientiSlice } from "./store/clienti"
+import { fattureSlice } from "./store/fatture"
 
 export default configureStore({
   reducer: {
     clienti: clientiSlice,
+    fatture: fattureSlice,
   },
 })
