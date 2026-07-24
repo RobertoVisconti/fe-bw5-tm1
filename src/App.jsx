32b197b6b1a29ac0f5a7ca471969c5ebeb0d1e3e
Roberto Visconti <robertovisconti93@gmail.com>
2026-07-24 09:23:22 +0200

    add route
    aggiunte rotte di navigazione

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Container } from "react-bootstrap";
 import Dashboard from "./components/Dashboard";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Container
         fluid
         className="d-flex flex-column min-vh-100 overflow-hidden p-0"
       >
         <header></header>
-        <main className="flex-grow-1 d-flex align-items-center justify-content-center">
-          <Dashboard />
-        </main>
+        <Routes>
+          <main className="flex-grow-1 d-flex align-items-center justify-content-center">
+            <Route path="/dashboard" element={<Dashboard />}></Route>
+          </main>
+        </Routes>
         <footer></footer>
       </Container>
-    </>
+    </BrowserRouter>
   );
 }
 
